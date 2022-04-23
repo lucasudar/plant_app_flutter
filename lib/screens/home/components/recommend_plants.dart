60fe40f9262806ef722a851165e3aa7a666a2a38
Nikita Starkov <nikita.starkov@tietoevry.com>
2022-04-23 14:09:15 -0700

    added icon_card

--- lib/screens/home/components/recommend_plants.dart
+++ lib/screens/home/components/recommend_plants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../constants.dart';
+import '../../details/details_screen.dart';
 
 class RecommendPlants extends StatelessWidget {
   const RecommendPlants({
@@ -17,21 +18,33 @@ class RecommendPlants extends StatelessWidget {
             title: 'Sakura',
             country: 'Russia',
             price: 400,
-            press: () {},
+            press: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const DetailsScreen();
+              }));
+            },
           ),
           RecommendPlantCard(
             image: 'assets/images/image_2.png',
             title: 'Aloe',
             country: 'Thailand',
             price: 50,
-            press: () {},
+            press: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const DetailsScreen();
+              }));
+            },
           ),
           RecommendPlantCard(
             image: 'assets/images/image_3.png',
             title: 'Ficus',
             country: 'Canada',
             price: 100500,
-            press: () {},
+            press: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const DetailsScreen();
+              }));
+            },
           ),
         ],
       ),
@@ -63,57 +76,58 @@ class RecommendPlantCard extends StatelessWidget {
         bottom: kDefaultPadding * 2.5,
       ),
       width: size.width * 0.4,
-      child: Column(
-        children: [
-          Image.asset(
-            image,
-          ),
-          GestureDetector(
-            onTap: press,
-            child: Container(
-                padding: const EdgeInsets.all(kDefaultPadding / 2),
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  ),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(0, 10),
-                      blurRadius: 50,
-                      color: kPrimaryColor.withOpacity(0.23),
-                    ),
-                  ],
+      child: GestureDetector(
+        onTap: press,
+        child: Column(
+          children: [
+            Image.asset(
+              image,
+            ),
+            Container(
+              padding: const EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
                 ),
-                child: Row(
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: '$title\n'.toUpperCase(),
-                            style: Theme.of(context).textTheme.button,
-                          ),
-                          TextSpan(
-                            text: country.toUpperCase(),
-                            style: TextStyle(
-                                color: kPrimaryColor.withOpacity(0.5)),
-                          ),
-                        ],
-                      ),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '$title\n'.toUpperCase(),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: country.toUpperCase(),
+                          style:
+                              TextStyle(color: kPrimaryColor.withOpacity(0.5)),
+                        ),
+                      ],
                     ),
-                    const Spacer(),
-                    Text(
-                      '\$$price',
-                      style: Theme.of(context).textTheme.button?.copyWith(
-                        color: kPrimaryColor,
-                      ),
-                    ),
-                  ],
-                )),
-          )
-        ],
+                  ),
+                  const Spacer(),
+                  Text(
+                    '\$$price',
+                    style: Theme.of(context).textTheme.button?.copyWith(
+                          color: kPrimaryColor,
+                        ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
